669b5266e925f479244b1fdbcc91c331b6a94fe4
hippieperm <leess0133@gmail.com>
2025-04-01 12:52:30 +0900

    테마 모드 변경: 라이트 테마 활성화 및 다크 테마의 슬라이더 속성 수정

--- lib/main.dart
+++ lib/main.dart
@@ -12,26 +12,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // themeMode: ThemeMode.light,
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.light,
+      // themeMode: ThemeMode.dark,
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.pinkAccent,
-            brightness: Brightness.light,
-          ),
-          dividerColor: Colors.black38,
-          sliderTheme: SliderThemeData(
-            thumbColor: Colors.pinkAccent,
-            activeTrackColor: Colors.black38,
-            inactiveTrackColor: Colors.black38,
-            trackHeight: 1,
-          )),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.pinkAccent,
+          brightness: Brightness.light,
+        ),
+        dividerColor: Colors.black38,
+        sliderTheme: SliderThemeData(
+          thumbColor: Colors.pinkAccent,
+          activeTrackColor: Colors.black38,
+          inactiveTrackColor: Colors.black38,
+          trackHeight: 1,
+        ),
+      ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.pinkAccent,
           brightness: Brightness.dark,
         ),
         dividerColor: Colors.white38,
+        sliderTheme: SliderThemeData(
+          thumbColor: Colors.pinkAccent,
+          activeTrackColor: Colors.white38,
+          inactiveTrackColor: Colors.white38,
+          trackHeight: 1,
+        ),
       ),
       home: HomePage(),
     );

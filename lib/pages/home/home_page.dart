d10cb84a5ff4204a21efd0bf95a38357cef22693
hippieperm <leess0133@gmail.com>
2025-04-01 15:30:20 +0900

    홈 페이지 UI 개선: SliderBox 위젯의 배치 변경 및 여백 추가로 사용자 경험 향상

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -12,20 +12,15 @@ class HomePage extends StatelessWidget {
         title: Text("BMI CALCULATOR"),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
         child: Column(
           children: [
             GenderBox(),
-            SliderBox(
-              lable: 'HEIGHT',
-              value: 170,
-              unit: 'cm',
-            ),
-            SliderBox(
-              lable: 'WEIGHT',
-              value: 70,
-              unit: 'kg',
-            ),
+            Spacer(),
+            SliderBox(lable: 'HEIGHT', value: 170, unit: 'cm'),
+            Spacer(),
+            SliderBox(lable: 'WEIGHT', value: 70, unit: 'kg'),
+            Spacer(),
             SizedBox(
               width: double.infinity,
               height: 56,

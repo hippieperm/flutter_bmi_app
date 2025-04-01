487908435646426b082f8242eafc17c7ebc6c860
hippieperm <leess0133@gmail.com>
2025-04-01 12:47:34 +0900

    테마 설정 업데이트: 라이트 및 다크 테마의 dividerColor 속성 추가 및 주석 처리된 테마 모드 수정

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
+      // themeMode: ThemeMode.dark,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.pinkAccent,
           brightness: Brightness.light,
         ),
+        dividerColor: Colors.black38,
       ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.pinkAccent,
           brightness: Brightness.dark,
         ),
+        dividerColor: Colors.white38,
       ),
       home: HomePage(),
     );
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -14,7 +14,11 @@ class HomePage extends StatelessWidget {
           Container(
             width: 100,
             height: 100,
-            decoration: BoxDecoration(border: Border.all()),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).dividerColor,
+              ),
+            ),
           ),
           Icon(Icons.male),
           Text('MALE')

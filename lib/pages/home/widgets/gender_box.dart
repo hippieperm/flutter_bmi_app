d92ff902831ef197e8f4aa30112a7bbde5c497d9
hippieperm <leess0133@gmail.com>
2025-04-01 15:50:45 +0900

    GenderBox 위젯 수정: 성별 선택 기능에 상태 관리 추가 및 UI 반영 개선

--- lib/pages/home/widgets/gender_box.dart
+++ lib/pages/home/widgets/gender_box.dart
@@ -1,15 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class GenderBox extends StatelessWidget {
-  const GenderBox({super.key});
+  final bool isMale;
+  void Function(bool newValue) onChanged;
+
+  GenderBox({
+    Key? key,
+    required this.isMale,
+    required this.onChanged,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        gender(context, Icons.male, 'MALE', true),
+        gender(context, Icons.male, 'MALE', isMale, true),
         SizedBox(width: 8),
-        gender(context, Icons.female, 'FEMALE', false),
+        gender(context, Icons.female, 'FEMALE', !isMale, false),
       ],
     );
   }
@@ -19,43 +27,49 @@ class GenderBox extends StatelessWidget {
     IconData icon,
     String text,
     bool selected,
+    bool isMaleBox,
   ) {
     return Expanded(
-      child: Container(
-        height: 150,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Theme.of(context).dividerColor,
-          ),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              top: 10,
-              left: 10,
-              child: Opacity(
-                opacity: 0.3,
-                child: Icon(
-                  icon,
-                  size: 80,
-                ),
-              ),
+      child: GestureDetector(
+        onTap: () {
+          onChanged(isMaleBox);
+        },
+        child: Container(
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).dividerColor,
             ),
-            Positioned(
-              right: 20,
-              bottom: 20,
-              child: Opacity(
-                opacity: selected ? 1 : 0.3,
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                top: 10,
+                left: 10,
+                child: Opacity(
+                  opacity: 0.3,
+                  child: Icon(
+                    icon,
+                    size: 80,
                   ),
                 ),
               ),
-            )
-          ],
+              Positioned(
+                right: 20,
+                bottom: 20,
+                child: Opacity(
+                  opacity: selected ? 1 : 0.3,
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

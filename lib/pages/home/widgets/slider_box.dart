d2c256e7f547957e18fdf534fc5eca5786072129
hippieperm <leess0133@gmail.com>
2025-04-01 15:27:12 +0900

    슬라이더 UI 구성: SliderBox 위젯 추가로 값 조정 기능 구현

---
+++ lib/pages/home/widgets/slider_box.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SliderBox extends StatelessWidget {
+  final String lable;
+  final double value;
+  final String unit;
+
+  const SliderBox({
+    super.key,
+    required this.lable,
+    required this.value,
+    required this.unit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              lable,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            Spacer(),
+            Text(
+              '$value',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              unit,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ],
+        ),
+        Slider(
+          value: value,
+          onChanged: (v) {},
+          min: 1,
+          max: 300,
+        )
+      ],
+    );
+  }
+}
